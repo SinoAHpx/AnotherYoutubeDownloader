5d4267a8dd11d5ea51356c8fe9c62edb8a611a1b
AHpx <ahpx@yandex.com>
2023-01-19 19:40:01 +0800

    Initialized

---
+++ AnotherYoutubeDownloader/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using Spectre.Console;
+
+AnsiConsole.Write(new FigletText("AYD"));
+AnsiConsole.MarkupLine($"Version {Assembly.GetExecutingAssembly().GetName().Version} by AHpx");
+AnsiConsole.MarkupLine($"GitHub: https://www.github.com/");
