003afe45c9dc25fdf824d88e0cd615801e7b1dc5
AHpx <ahpx@yandex.com>
2023-01-19 20:40:31 +0800

    Simplified testing

--- AnotherYoutubeDownloader/Program.cs
+++ AnotherYoutubeDownloader/Program.cs
@@ -1,30 +1,40 @@
 ﻿using System.Reflection;
 using Spectre.Console;
+using YoutubeExplode;
 
 AnsiConsole.Write(new FigletText("AYD"));
 AnsiConsole.MarkupLine($"Version {Assembly.GetExecutingAssembly().GetName().Version} by AHpx");
 AnsiConsole.MarkupLine($"GitHub: https://github.com/SinoAHpx/AnotherYoutubeDownloader");
 
+var ytClient = new YoutubeClient();
 while (true)
 {
+    #if DEBUG
+    var url = "https://www.youtube.com/watch?v=ANIX12gCtyM";
+    #else
     var url = AnsiConsole.Ask<string>("Input a url or video or playlist:");
-    // if (!Uri.IsWellFormedUriString(url, UriKind.Absolute) || !url.Contains("youtube.com"))
-    // {
-    //     AnsiConsole.MarkupLine("[red]Invalid url, please input again[/]");
-    //     continue;
-    // }
-
-    AnsiConsole.Progress().Start(c =>
+    #endif
+    
+    if (!Uri.IsWellFormedUriString(url, UriKind.Absolute) || !url.Contains("youtube.com"))
     {
-        var task1 = c.AddTask("Downloading audio");
-        var task2 = c.AddTask("Downloading video");
-        while (!c.IsFinished)
-        {
-            Thread.Sleep(1);
-            task1.Increment(1);
-            task2.Increment(1);
-        }
-    });
+        AnsiConsole.MarkupLine("[red]Invalid url, please input again[/]");
+        continue;
+    }
+
+    var video = await ytClient.Videos.GetAsync(url);
+    // ytClient.Videos.Streams.GetAsync();
+
+    // AnsiConsole.Progress().Start(c =>
+    // {
+    //     var task1 = c.AddTask("Downloading audio");
+    //     var task2 = c.AddTask("Downloading video");
+    //     while (!c.IsFinished)
+    //     {
+    //         Thread.Sleep(1);
+    //         task1.Increment(1);
+    //         task2.Increment(1);
+    //     }
+    // });
     
     AnsiConsole.MarkupLine("Download completed");
 }
