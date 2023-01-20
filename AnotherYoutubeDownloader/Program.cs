13f15b908e0509b71d1b3ec0b4ee23e57793260d
AHpx <ahpx@yandex.com>
2023-01-20 15:28:01 +0800

    Accomplished

--- AnotherYoutubeDownloader/Program.cs
+++ AnotherYoutubeDownloader/Program.cs
@@ -1,40 +1,87 @@
 ﻿using System.Reflection;
+using Manganese.Text;
+using Polly;
 using Spectre.Console;
 using YoutubeExplode;
+using YoutubeExplode.Videos;
+using YoutubeExplode.Videos.Streams;
 
 AnsiConsole.Write(new FigletText("AYD"));
-AnsiConsole.MarkupLine($"Version {Assembly.GetExecutingAssembly().GetName().Version} by AHpx");
+AnsiConsole.MarkupLine($"[red]Version {Assembly.GetExecutingAssembly().GetName().Version} by AHpx[/]");
 AnsiConsole.MarkupLine($"GitHub: https://github.com/SinoAHpx/AnotherYoutubeDownloader");
 
 var ytClient = new YoutubeClient();
+//https://www.youtube.com/watch?v=ANIX12gCtyM
 while (true)
 {
-    #if DEBUG
-    var url = "https://www.youtube.com/watch?v=ANIX12gCtyM";
-    #else
     var url = AnsiConsole.Ask<string>("Input a url or video or playlist:");
-    #endif
     
     if (!Uri.IsWellFormedUriString(url, UriKind.Absolute) || !url.Contains("youtube.com"))
     {
         AnsiConsole.MarkupLine("[red]Invalid url, please input again[/]");
-        continue;
+        return ;
     }
 
-    var video = await ytClient.Videos.GetAsync(url);
-    // ytClient.Videos.Streams.GetAsync();
+    var videoMetadata = await ytClient.Videos.GetAsync(url);
+    AnsiConsole.MarkupLine($"Title: [red]{videoMetadata.Title}[/]");
+    AnsiConsole.MarkupLine($"Author: [red]{videoMetadata.Author.ChannelTitle}[/]");
 
-    // AnsiConsole.Progress().Start(c =>
-    // {
-    //     var task1 = c.AddTask("Downloading audio");
-    //     var task2 = c.AddTask("Downloading video");
-    //     while (!c.IsFinished)
-    //     {
-    //         Thread.Sleep(1);
-    //         task1.Increment(1);
-    //         task2.Increment(1);
-    //     }
-    // });
+    var rawLocation = AnsiConsole.Confirm("Download to default location[yellow](./ayd)[/]? ")
+        ? "./ayd"
+        : AnsiConsole.Ask<string>("Where do you want?");
+
+    if (rawLocation.IsNullOrEmpty())
+        return;
+
+    var downloadDirectory = Directory.CreateDirectory(rawLocation);
+    var downloadLocation = GetExactLocation(videoMetadata, downloadDirectory);
+
+    if (downloadLocation.Exists)
+    {
+        if (AnsiConsole.Confirm($"{downloadLocation.Name} already exists, overwrite?"))
+            downloadLocation.Delete();
+        else
+            return;
+    }
+
+    await Policy.Handle<Exception>()
+        .RetryAsync(3, (exception, i) =>
+        {
+            AnsiConsole.MarkupLine($"{exception.GetType().Name} occurred: {exception.Message}, retrying for {i} times");
+        })
+        .ExecuteAsync(async () =>
+        {
+            await AnsiConsole.Progress().StartAsync(async ctx =>
+            {
+                var t1 = ctx.AddTask(videoMetadata.Title, maxValue: 1);
+                var streamManifest = await ytClient.Videos.Streams.GetManifestAsync(videoMetadata.Id);
+                var stream = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+                var progress = new Progress<double>();
+                progress.ProgressChanged += (_, args) =>
+                {
+                    t1.Value = args;
+                };
+
+                await ytClient.Videos.Streams.DownloadAsync(stream, downloadLocation.FullName, progress);
+            });
+        });
     
+
     AnsiConsole.MarkupLine("Download completed");
+}
+
+FileInfo GetExactLocation(Video videoMetadata, DirectoryInfo downloadDirectory)
+{
+    var fileName = $"{videoMetadata?.Author.ChannelTitle} - {videoMetadata?.Title}.mp4";
+    var filePath = Path.Combine(downloadDirectory!.FullName, fileName);
+    return new FileInfo(filePath);
+} 
+
+public static class Extensions
+{
+    public static T Print<T>(this T t)
+    {
+        Console.WriteLine(t);
+        return t;
+    }
 }
